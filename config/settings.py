0abee23c2afb82d04ebda7d8f51da093dced8dd3
gimgisu <gisu1102@gmail.com>
2025-01-13 00:46:12 +0900

    fix :: localhost 수정

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME", "postgres"),
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
-        "HOST": os.getenv("DB_HOST", "postgres"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", 5432),
     }
 }
