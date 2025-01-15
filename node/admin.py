1dd22b59060cc6106f2d3e317dd3b1b1edaf9660
Jimin15 <sujean07015@gmail.com>
2025-01-15 21:05:37 +0900

    노드 admin

--- node/admin.py
+++ node/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Node
 
-# Register your models here.
+class NodeAdmin(admin.ModelAdmin):
+    # 필드를 지정하여 Admin 화면에서 표시할 항목을 설정
+    list_display = ('node_id', 'name', 'created_at', 'is_deleted', 'deleted_at', 'node_img')
+
+admin.site.register(Node, NodeAdmin)
