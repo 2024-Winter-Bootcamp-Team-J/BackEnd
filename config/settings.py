63b8bd7e521d85a14c1bf5c1f72ae468d22fa334
gimgisu <gisu1102@gmail.com>
2025-01-16 00:07:49 +0900

    fix :: 연결 데이터베이스 Docker 내부 PostgreSql 매핑

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME", "postgres"),
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
+        "HOST": os.getenv("DB_HOST", "postgres"),
         "PORT": os.getenv("DB_PORT", 5432),
     }
 }
