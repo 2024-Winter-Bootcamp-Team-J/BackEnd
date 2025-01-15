fa53905a621aeb3241ae5dacd3ce31026bd6283c
Jimin15 <sujean07015@gmail.com>
2025-01-15 21:04:50 +0900

    config-수정-오류해결 관련
    path('', home) 이거 없어서 추가

--- config/urls.py
+++ config/urls.py
@@ -1,27 +1,13 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.http import HttpResponse  # 임시로 홈 페이지 뷰를 작성
+
+# 루트 URL에 대한 간단한 응답을 추가
 def home(request):
-    return HttpResponse("Welcome to the Memo App!")  # 간단한 메시지 출력
+    return HttpResponse("Welcome to the homepage!")
 
 # Swagger 설정
 schema_view = get_schema_view(
@@ -37,8 +23,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('memo.urls')),
-path('api/', include('node.urls')),  # node 앱의 URL 연결
+    path('', home),  # 루트 URL에 홈 페이지 뷰 연결
+    path('api/', include('memo.urls')),  # memo 앱의 URL 연결
+    path('api/', include('node.urls')),  # node 앱의 URL 연결
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
