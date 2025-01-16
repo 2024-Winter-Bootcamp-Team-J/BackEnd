5b1403e7cb21a8a6341137ffac7a0bb2c5a3434d
chlwoomin@naver.com <chlwoomin@naver.com>
2025-01-16 22:10:18 +0900

    system에 있던 프롬프트를 user로 내리고 체계적으로 조금 더 명확하게 전달달

--- name_extract_API.py
+++ name_extract_API.py
@@ -1,16 +1,23 @@
 import openai
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # API 키 설정
 def name_extract(text):
     openai.api_key = os.getenv('OPENAI_API_KEY')
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "너는 입력에 대해 사람 이름만을 각각 추출하고 그룹화 하는 챗봇이야. \
-            사람 이름이 없으면 none라고 대답해. 여러 사람이 등장하면 같은 무리에 속하는지 그룹을 판단하고 전부 추출해야 해. \
-            예를 들어 '오전에는 윤서랑 밥을 먹고 오후에는 우민이랑 영화를 봤다' 라는 입력이 들어오면 group1 : ""우민"", group2 : ""김철수""를 추출해야 해. \
-            문맥을 파악해서 사람이름을 판단하는게 중요해 그래서 문장을 잘 읽어보고 답해줘. 일반명사와 사람이름을 잘 구분해야돼. 예시에서 밥은 사람이 아니야. 인칭대명사는 제외해"},
-            {"role": "user", "content": f'{text} 출력은 json형식으로 추출해줘.'},
+            {"role": "system", "content": "너는 입력된 텍스트에서 사람 이름만을 추출하고 이를 그룹화하는 챗봇이야."},            
+            {"role": "user", "content": f'{text} json형식으로 아래 규칙에 따라서 추출해줘 \
+             1. 입력된 문장에서 사람이름이 등장하지 않으면 none이라고 답해야 해.\
+             2. 만약 여러 명의 사람이 등장한다면, 그들이 같은 활동을 했거나 같은 문맥에서 등장하면 같은 그룹으로 묶어야 해.\
+             3. 예를 들어 오전에는 윤서랑 밥을 먹고 오후에는 우민이랑 영화를 봤다 라는 문장이 주어지면, \
+                group1: 윤서, group2: 에 우민 이 출력이야야. \
+             4. 문장을 잘 분석해서 사람이름을 정확히 식별하고 일반명사와 구분하는 것이 중요해. \
+             4.1 밥, 영화처럼 사람이 아닌 일반 명사는 이름에서 제외해야 해. \
+             5. 인칭대명사(예: 나, 너, 그, 그녀)는 이름에서 제외해야 해. \
+             6. 결과는 항상 JSON 형식으로 반환해야 해'},
         ]
     )
 
@@ -22,4 +29,4 @@ def name_extract(text):
     return message
 
 # 테스트용
-# name_extract(input())
+name_extract(input())
