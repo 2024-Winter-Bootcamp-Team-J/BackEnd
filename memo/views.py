8b8751ea4b4c77f34e7e082ca45a1a531e1887c9
woomin <chlwoomin@naver.com>
2025-01-17 17:29:51 +0900

    복구5

--- memo/views.py
+++ memo/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Memo
 from .serializers import MemoCreateSerializer, MemoResponseSerializer, MemoRetrieveSerializer
+from rest_framework.response import Response
 
 # 메모 생성을 처리하는 뷰
 class MemoCreateView(generics.CreateAPIView):
@@ -14,10 +15,14 @@ class MemoCreateView(generics.CreateAPIView):
         """
         POST 요청 시 메모를 생성하고 성공 메시지를 반환합니다.
         """
-        response = super().create(request, *args, **kwargs)  # 부모 클래스의 create 메서드 호출
+        # 직렬화된 데이터를 저장
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        memo = serializer.save()  # 메모 생성
+        
         # 성공 응답 데이터 생성
-        success_response = MemoResponseSerializer({'status': 'success'})
-        return Response(success_response.data, status=response.status_code)
+        success_response = MemoResponseSerializer({'status': 'success', 'memo_id': memo.memo_id})
+        return Response(success_response.data, status=201)  # 성공 응답 반환
 
 # 특정 메모를 조회하는 뷰
 class MemoRetrieveView(generics.RetrieveAPIView):
