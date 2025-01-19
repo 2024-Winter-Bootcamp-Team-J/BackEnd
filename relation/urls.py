587d4d2499cf4e012a9d607fdd3af9b3741d7e54
woomin <chlwoomin@naver.com>
2025-01-19 15:06:54 +0900

    url 조금 더 수정

--- relation/urls.py
+++ relation/urls.py
@@ -7,7 +7,7 @@ from .views import (
 
 urlpatterns = [
     path('', GetRelationsView.as_view(), name='get_relations'),
-    path('/create', CreateRelationView.as_view(), name='create_relation'),
+    path('create', CreateRelationView.as_view(), name='create_relation'),
     path('relation-types', ListRelationTypesView.as_view(), name='list_relation_types'),
     path('relation-types/create', CreateRelationTypeView.as_view(), name='create_relation_type'),
     path('relation-types/<int:pk>', GetRelationTypeView.as_view(), name='get_relation_type'),
