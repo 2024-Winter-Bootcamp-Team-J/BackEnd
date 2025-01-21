0d09b50d3a2c62877217b4e0d701aef50b8ac10e
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2025-01-21 12:04:57 +0900

    사용자 글 작성 모델

--- controller/models.py
+++ controller/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Write(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
