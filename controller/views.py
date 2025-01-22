60f05c5b8ad38ba91b1def02380a0692f6a05dc4
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2025-01-22 22:25:44 +0900

    controller/views.py 제목 변경

--- controller/views.py
+++ controller/views.py
@@ -7,11 +7,7 @@ from .models import Write
 from .serializers import WriteSerializer
 from .name_extract_API import name_extract
 
-class WriteCreateView(APIView):
-    def get(self, request):
-        writes = Write.objects.all()
-        serializer = WriteSerializer(writes, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class ControllerView(APIView):
 
     @swagger_auto_schema(
         request_body=WriteSerializer,
