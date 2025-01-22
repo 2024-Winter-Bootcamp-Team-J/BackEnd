e6c3d8f531d10ca70a7757fd770fcb18a5d862e6
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2025-01-22 22:33:27 +0900

    search 노드 검색

--- search/documents.py
+++ search/documents.py
@@ -1,9 +1,33 @@
 from django_opensearch_dsl import Document
 from opensearch_dsl import Document
 from memo.models import Memo
+from node.models import Node
 
 #model과 document를 연결하는 곳
 #document는 Opensearch에 저장할 데이터를 정의
+
+class NodeDocument(Document):
+    class Index:
+        name = 'node'
+
+    class Django:
+        model = Node  # Node 모델을 참조하여 매핑
+
+        # Node 모델에서 정의된 필드를 Openearch 필드로 자동 매핑
+        fields = [
+            'node_id',
+            'user',
+            'name',
+            'created_at',
+            'is_deleted',
+            'deleted_at',
+            'node_img',
+        ]
+    # 노드 검색을 위한 메소드
+    @classmethod
+    def search_node(cls, query):
+        # content 필드에 대해 full-text search
+        return cls.search().filter('match', name=query)
 
 class MemoDocument(Document):
     class Index:
--- search/signals.py
+++ search/signals.py
@@ -1,7 +1,30 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from node.models import Node
+from .documents import NodeDocument
 from memo.models import Memo
 from .documents import MemoDocument
+
+# Node 객체 저장 시 OpenSearch에 인덱싱
+@receiver(post_save, sender=Node)
+def index_node(sender, instance, created, **kwargs):
+    if created:
+        # NodeDocument 인스턴스 생성 및 OpenSearch에 저장
+        node_document = NodeDocument(
+            meta={'id': instance.node_id},  # OpenSearch의 고유 ID
+            node_id=instance.node_id,
+            name=instance.name,
+            created_at=instance.created_at,
+            deleted_at=instance.deleted_at,
+            is_deleted=instance.is_deleted,
+        )
+        # OpenSearch에 저장
+        node_document.save(index='node')
+
+# Node 객체 삭제 시 OpenSearch에서 해당 데이터 삭제
+@receiver(post_delete, sender=Node)
+def delete_node_from_index(sender, instance, **kwargs):
+    NodeDocument.get(id=instance.node_id).delete()
 
 # memo앱의 save이벤트가 발생하면 index_memo함수 실행
 @receiver(post_save, sender=Memo)
@@ -11,7 +34,6 @@ def index_memo(sender, instance, created, **kwargs):
         memo_document = MemoDocument(meta={'id': instance.memo_id})
         memo_document.memo_id = instance.memo_id
         memo_document.node_id = instance.node_id
-        memo_document.user_id = instance.user_id
         memo_document.content = instance.content
         memo_document.is_deleted = instance.is_deleted
         memo_document.created_at = instance.created_at
