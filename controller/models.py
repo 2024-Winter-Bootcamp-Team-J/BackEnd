77cc5384b66576f41a89c0e0aaf174f6d31aa2d0
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2025-01-23 16:22:09 +0900

    Revert "사용자 글 작성 모델"
    This reverts commit 0d09b50d3a2c62877217b4e0d701aef50b8ac10e.

--- controller/models.py
+++ controller/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
-class Write(models.Model):
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
+# Create your models here.
