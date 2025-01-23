f85944a7803811fcdb2610376e678122e0facdd5
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2025-01-23 16:22:08 +0900

    Revert "controller 불필요 코드 삭제, 노드 검색+노드 생성"
    This reverts commit ae32a2281924789731b347873ec06470ea49efee.

--- controller/views.py
+++ controller/views.py
@@ -29,46 +29,13 @@ class ControllerView(APIView):
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
-            # 그룹(이벤트)별 node 검색 및 생성
-            nodes_result = {}
-            for group, names in extracted_names.items():
-                nodes_result[group] = []  # 그룹별 Node 정보를 저장
-                for name in names:
-                    # Node 검색 (OpenSearch)
-                    search_results = NodeDocument.search().query("match", name=name).execute()
-                    if search_results.hits:
-                        # 검색 결과가 있으면 Node 정보를 저장
-                        node_result = search_results[0]
-                        node_data = {
-                            "name": node_result.name,
-                            "node_id": node_result.node_id,
-                        }
-                    else:
-                        node_serializer = NodeCreateSerializer(data={"name": name, "user": write.user.user_id, "node_img": None})
-                        if node_serializer.is_valid():  # 유효성 검사
-                            node_create_result = node_serializer.save()  # 저장
-                            node_data = node_serializer.to_representation(node_create_result)
-                        else:
-                            nodes_result[group].append({
-                                "error": "노드 생성 실패",
-                                "details": node_serializer.errors,
-                                "name": name,
-                            })
-                            continue  # 다음 이름으로 이동
-
-                            # 성공적으로 생성된 노드를 결과에 추가
-                        nodes_result[group].append(node_data)
-
-                        # 최종 결과 반환
-                        return Response(
-                            {
-                                "message": "글 작성, 이름 추출, Node 및 메모 처리 완료",
-                                "write": serializer.data,
-                                "extracted_names": extracted_names,
-                                "nodes": nodes_result,
-                            },
-                            status=status.HTTP_201_CREATED,
-                        )
-
-
+                # 저장된 데이터와 이름 추출 결과 반환
+            return Response(
+                {
+                    "message": "글 작성 및 이름 추출 완료",
+                    "write": serializer.data,
+                    "extracted_names": extracted_names,
+                },
+                status=status.HTTP_201_CREATED,
+            )
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
