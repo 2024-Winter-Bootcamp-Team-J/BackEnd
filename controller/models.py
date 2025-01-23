268a6ffae5a09933542fc35e8abb4ee930500ba7
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2025-01-24 00:54:46 +0900

    컨트롤러 앱 생성 - 글 작성 추가

---
+++ controller/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Write(models.Model):
+    controller_id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
