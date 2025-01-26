16a220e31ab6093f72c32b438ceba1715d66a249
Jimin15 <sujean07015@gmail.com>
2025-01-27 04:59:04 +0900

    default relation type데이터 지정 ['친구', '가족', '게임', '지인', '직장']

--- relation/apps.py
+++ relation/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class RelationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relation'
+
+    def ready(self):
+        from .models import RelationType
+        from django.db import connection
+
+        def create_default_relation_types(sender, **kwargs):
+            if 'relation_relationtype' in connection.introspection.table_names():
+                default_types = ['친구', '가족', '게임', '지인', '직장']
+                for name in default_types:
+                    RelationType.objects.get_or_create(name=name, user_id=None, is_default=True)
+
+        post_migrate.connect(create_default_relation_types, sender=self)
