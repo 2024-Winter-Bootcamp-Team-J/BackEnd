a6f0cde27324ffc4b56a401b8a457490ce065c6d
gimgisu <gisu1102@gmail.com>
2025-01-27 19:34:25 +0900

    chore :: makemigrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # 로그 출력을 위해 스크립트 시작 시 메시지 출력
 echo "Starting entrypoint script..."
 
+python manage.py makemigrations
 # Django migration 자동화
 echo "Running migrations..."
 python manage.py migrate || { echo "Migration failed"; exit 1; }
